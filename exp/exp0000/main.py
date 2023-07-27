2dc963fd36a479ac589896b69c150f0e17cc6fc3
Luka-Magic <74438554+Luka-Magic@users.noreply.github.com>
2023-07-27 09:22:44 +0900

    0

--- exp/exp0000/main.py
+++ exp/exp0000/main.py
@@ -529,6 +529,10 @@ def main(
             scheduler = optim.lr_scheduler.OneCycleLR(
                 optimizer, total_steps=cfg.n_epochs * len(train_loader), max_lr=cfg.lr, pct_start=cfg.pct_start, div_factor=cfg.div_factor, final_div_factor=cfg.final_div_factor)
             scheduler_step_frequence = cfg.scheduler_step_frequence
+        elif cfg.scheduler == 'CosineAnnealingWarmRestarts':
+            optim.lr_scheduler.CosineAnnealingWarmRestarts(
+                optimizer, cfg.T_0, T_mult=cfg.T_multi, eta_min=cfg.eta_min)
+            scheduler_step_frequence = cfg.scheduler_step_frequence
         else:
             scheduler = None
 
