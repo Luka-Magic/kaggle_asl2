3aa6ff2759605edaeef42d336f8e708355158a4b
Luka-Magic <74438554+Luka-Magic@users.noreply.github.com>
2023-08-12 09:58:00 +0900

    1 debug

--- exp/exp0001/main.py
+++ exp/exp0001/main.py
@@ -486,6 +486,7 @@ def train_function(
         dec_self_attn_msk = batch['dec_self_attn_msk'].to(device)
         dec_cross_attn_msk = batch['dec_cross_attn_msk'].to(device)
         with autocast():
+            print(hand.shape, input_label.shape)
             preds = model(hand, input_label, enc_self_attn_msk, dec_self_attn_msk,
                           dec_cross_attn_msk)
             loss = loss_fn(preds, targets)
