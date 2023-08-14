3af6b6893deb6dae21c67eb66673ebedf3a39483
Luka-Magic <74438554+Luka-Magic@users.noreply.github.com>
2023-08-14 20:55:22 +0900

    5

--- exp/exp0005/main.py
+++ exp/exp0005/main.py
@@ -228,8 +228,8 @@ def pre_process1(lip, rhand, lhand, rpose, lpose):
     lpose = (resize_pad(lpose) - LPM) / LPS
     rhand_diff = resize_pad(rhand_diff)
     lhand_diff = resize_pad(lhand_diff)
-    rhand_dist = resize_pad(rhand_dist)
-    lhand_dist = resize_pad(lhand_dist)
+    rhand_dist = resize_pad(rhand_dist)[:, :, 0]
+    lhand_dist = resize_pad(lhand_dist)[:, :, 0]
 
     x = tf.concat([lip, rhand, lhand, rpose, lpose,
                   rhand_diff, lhand_diff], axis=1)
