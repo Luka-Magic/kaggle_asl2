59ef23d22866ac638a695c69387d3321a909a9fc
Luka-Magic <74438554+Luka-Magic@users.noreply.github.com>
2023-08-20 17:14:34 +0900

    15

--- exp/exp0015/main.py
+++ exp/exp0015/main.py
@@ -19,8 +19,8 @@ warnings.filterwarnings('ignore')
 # ====================================================
 DEBUG = False
 RESTART = True
-best_epoch = 23
-best_score = 0.6624
+best_epoch = 46
+best_score = 0.6930
 # ====================================================
 
 SEED = 77
