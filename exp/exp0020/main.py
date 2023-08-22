1492f84729ac63f80f3588e25806ceb691cda616
Luka-Magic <74438554+Luka-Magic@users.noreply.github.com>
2023-08-23 07:29:03 +0900

    20

--- exp/exp0020/main.py
+++ exp/exp0020/main.py
@@ -21,8 +21,8 @@ warnings.filterwarnings('ignore')
 # ====================================================
 DEBUG = False
 RESTART = True
-best_epoch = 18
-best_score = 0.6421
+best_epoch = 37
+best_score = 0.6782
 # ====================================================
 use_wandb = int(sys.argv[1])
 
